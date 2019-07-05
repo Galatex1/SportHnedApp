9c94e15592a110cf00d61f1df035b85645696156
Petr <petrmachu@icloud.com>
2019-07-05 17:52:09 +0200

    pokusil jsem se přidat mapu.

--- components/SportsScreen/SportsScreen.js
+++ components/SportsScreen/SportsScreen.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
-import MapView from 'react-native-maps';
+import MapView from 'react-native-maps'
 
 export default class SportScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <MapView
-            initialRegion={{
-              latitude: 37.78825,
-              longitude: -122.4324,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
-            }}
-        />
-      </View>
+        <MapView style={{flex: 1}} />
     );
   }
 }
